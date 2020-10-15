4360a8e42698fab3d3f4f78c29f92e15dbdfaaba
Saul Castillo <secch97@gmail.com>
2020-10-15 01:58:10 -0600

    Leve modificaciones

---
+++ RestauranteWeb/Controllers/RestauranteController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RestauranteWeb.Controllers
+{
+    public class RestauranteController : Controller
+    {
+        // GET: Restaurante
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
