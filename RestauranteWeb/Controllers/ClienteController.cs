0157e5bd4006a0808885e88e913c99ee2b27dcae
Saul Castillo <secch97@gmail.com>
2020-10-15 01:33:33 -0600

    Creación de proyecto de catedra

---
+++ RestauranteWeb/Controllers/ClienteController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RestauranteWeb.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        public ActionResult Inicio()
+        {
+            //Insertar aca código para mostrar en página principal.
+            return View();
+        }
+
+    }
+}
